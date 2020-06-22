602131e3b9859e5c7b180834da5cb9d409b5ab81
itay <itaymeshulam2@gmail.com>
2020-06-22 19:33:19 +0300

    itay

--- app/src/main/java/com/example/spacedog/MainActivity.java
+++ app/src/main/java/com/example/spacedog/MainActivity.java
@@ -14,7 +14,6 @@ import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
